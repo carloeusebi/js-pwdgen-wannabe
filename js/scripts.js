6ffe5b6e2f16dc549081f04aaf5de6e432e58c17
carloeusebi <carloeusebi@gmail.it>
2023-05-25 14:12:06 +0200

    Improved comments

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,16 @@ const pwdNumber = '21';
 const pwdPlaceholder = document.getElementById('password');
 console.log(pwdPlaceholder);
 
+// # Collecting user inputs
+
+// Firstname
 const userFirstName = prompt('Inserisci il nome', 'Carlo');
 console.log(userFirstName);
 
+// Lastname
 const userLastName = prompt('Inserisci il tuo cognome', 'Eusebi');
 console.log(userLastName);
 
+// Colors
 const userFavColor = prompt('Inserisci il tuo colore preferito', 'Blallo');
 console.log(userFavColor);
